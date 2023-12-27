7377218e3bf59ff60ab4e9fe68f9bf62b322badd
Alex Smith <109144005+AlexMSm@users.noreply.github.com>
2023-12-27 12:58:21 +0000

    Adding profile header compoent

---
+++ components/shared/ProfileHeader.tsx
@@ -0,0 +1,16 @@
+interface Props {
+    accountId: string;
+    authUserId: string;
+    name: string;
+    username: string;
+    imgUrl: string;
+    bio: string;
+}
+
+const ProfileHeader = ({accountId, authUserId, name, username, imgUrl, bio} : Props) => {
+    return  (
+        <div><h1>Profile header</h1></div>
+    )
+}
+
+export default ProfileHeader;
