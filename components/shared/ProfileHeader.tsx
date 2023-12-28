dd96056635a400eb9e8b8a0502e819a1dab8c18f
Alex Smith <109144005+AlexMSm@users.noreply.github.com>
2023-12-28 12:27:06 +0000

    Set up tabs on profile page

--- components/shared/ProfileHeader.tsx
+++ components/shared/ProfileHeader.tsx
@@ -27,7 +27,11 @@ const ProfileHeader = ({accountId, authUserId, name, username, imgUrl, bio} : Pr
                         <p className="text-base-medium text-gray-1">@{username}</p>
                     </div>
                 </div>
-            </div>
+                </div>
+                {/*TODO Community */}
+                <p className="mt-6 max-w-lg text-base-regular text-light-2">{bio}</p>
+                <div className="mt-12 h-0.5 w-full bg-dark-3"/>
+            
         </div>
     )
 }
