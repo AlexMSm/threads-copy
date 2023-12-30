b88aeb585384ba5edb53215b81e19e491a7dc49b
Alex Smith <109144005+AlexMSm@users.noreply.github.com>
2023-12-30 20:50:15 +0000

    added activity notification page

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,8 +1,51 @@
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { fetchUser, fetchUsers, getActivity } from "@/lib/actions/user.actions";
+import Link from "next/link";
+import Image from "next/image";
+
 
 const Page = async () => {
+    
+    const user = await currentUser();
+    if (!user) return null;
+
+    // fetch organization list created by user
+    const userInfo = await fetchUser(user.id);
+    if (!userInfo?.onboarded) redirect("/onboarding");
+
+    const activity = await getActivity(userInfo._id)
+
     return (
       <section>
           <h1 className="head-text mb-10">Activity</h1>
+
+        <section className="mt-110 fkex fkex-col gap-5">
+          {activity.length > 0 ? (
+          <>
+          {activity.map((activity) => (
+            <Link key={activity._id} href={`/thread/${activity.parentId}`}>
+              <article className="activity-card">
+                <Image 
+                  src={activity.author.image}
+                  alt="Profile Picture"
+                  height={20}
+                  width={20}
+                  className="rounded-full object-cover"/>
+                  <p className="!text-small-regular text-light-1">
+                    <span className="mr-1 text-primary-500">
+                      {activity.author.name}
+                    </span>{" "} replied to your thread
+                  </p>
+              </article>
+            </Link>
+          ))}
+          </>
+          ) : <p className="!text-base-regular text-light-3">No activity yet</p> 
+          }
+        
+        </section>
+
       </section>
     )
   }
--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -19,8 +19,6 @@ const Page = async ({
     const user = await currentUser();
     if (!user) return null;
 
-   
-
     // fetch organization list created by user
     const userInfo = await fetchUser(user.id);
     if (!userInfo?.onboarded) redirect("/onboarding");
